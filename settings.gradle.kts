8e060f39f80022cd159c4e7b0c2d2d8e63d8aef8
BaziakKamil <baziakkamil@gmail.com>
2025-02-08 17:02:05 +0100

    update from groovy to kts

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+rootProject.name = "Car Cost Notebook"
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":app")
