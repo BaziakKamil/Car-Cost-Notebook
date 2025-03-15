c60fabc5a78830f91ab0a662aaf3c13d218d293a
BaziakKamil <baziakkamil@gmail.com>
2025-03-15 21:38:18 +0100

    working branch update

--- app/src/main/java/pl/kamilbaziak/carcostnotebook/ui/compose/activity/ComposeActivity.kt
+++ app/src/main/java/pl/kamilbaziak/carcostnotebook/ui/compose/activity/ComposeActivity.kt
@@ -22,7 +22,7 @@ class ComposeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
